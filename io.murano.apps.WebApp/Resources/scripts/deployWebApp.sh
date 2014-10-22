ff4ef806c04a65714e73890f86329ed858210310
Serg Melikyan <serg.melikyan@gmail.com>
2014-10-22 19:59:34 +0400

    Update deployWebApp.sh

--- io.murano.apps.WebApp/Resources/scripts/deployWebApp.sh
+++ io.murano.apps.WebApp/Resources/scripts/deployWebApp.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# Install Apache WebSerber
 yum -y install httpd
+
+# Add HTML Page with machine ID
 echo "Instance: ${1}" > /var/www/html/index.html
+
+# Restart Apache to apply changes
 systemctl start httpd
 systemctl enable httpd
+
+# Install and configure local firewall
 yum -y install firewalld
 systemctl unmask firewalld
 systemctl enable firewalld
